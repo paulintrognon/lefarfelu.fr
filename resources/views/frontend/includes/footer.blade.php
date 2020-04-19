e2bf49aecb574ef960eddbc3a55dc7aa3fe22dcf
Paulin Trognon <paulin.trognon@gmail.com>
2020-04-19 13:41:41 +0200

    fix footer

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -4,6 +4,6 @@
     </div>
     <div class="Footer-text">
         <span class="has-text-primary">Une question ?</span> Le bureau du camp du Farfelu peut vous aider !<br/>
-        Contactez Claire, Nanon ou Hélène par email à <span class="has-text-primary">farfelu@gmail.com</span>
+        Contactez Claire, Nanon ou Hélène par email à <span class="has-text-primary">campfarfelu@gmail.com</span>
     </div>
 </footer>
